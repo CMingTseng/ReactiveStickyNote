ad1f7931f8d9f6ec6bcf033c76aaf9f7ba4c37fd
Neo <c.min.tseng@gmail.com>
2021-09-11 13:09:03 +0800

    remove un-use

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,8 +6,3 @@ repositories {
     mavenCentral()
     google()
 }
-
-dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
-    implementation("com.android.tools.build:gradle:7.1.0-alpha03")
-}
