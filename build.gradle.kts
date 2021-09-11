60c92eb27860942f8d5643ad3d08da2b89025c67
Neo <c.min.tseng@gmail.com>
2021-09-11 13:22:28 +0800

    modify org.jetbrains.kotlin:kotlin-gradle-plugin to kotlin("gradle-plugin") <-- JB office type !!

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
     dependencies {
         classpath("com.google.gms:google-services:4.3.8")
         classpath("com.android.tools.build:gradle:7.1.0-alpha05")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${com.yanbin.reactivestickynote.build.Version.kotlin}")
+        classpath(kotlin("gradle-plugin", ${com.yanbin.reactivestickynote.build.Version.kotlin}))
     }
 }
 
