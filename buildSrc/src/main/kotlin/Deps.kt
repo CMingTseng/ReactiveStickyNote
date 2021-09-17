753e5a5540f7e18c30541cf5ceee1a00ef06cec7
Neo <c.min.tseng@gmail.com>
2021-09-17 14:05:57 +0800

    upgrade kotlin to 1.5.30 build fail e: This version (1.0.1) of the Compose Compiler requires Kotlin version 1.5.21 but you appear to be using Kotlin version 1.5.30 which is not known to be compatible.  Please fix your configuration (or `suppressKotlinVersionCompatibilityCheck` but don't say I didn't warn you!).
    FAILURE: Build failed with an exception.

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -39,7 +39,7 @@ object Deps {
 object Version {
     const val agp = "7.0.1"
     //FIXME compose 1.0.0 <--> kotlin 1.5.10  &  compose 1.0.1 <--> kotlin 1.5.21
-    const val kotlin = "1.5.21"
+    const val kotlin = "1.5.30"
     const val jUnit5 = "5.3.1"
     const val compose = "1.0.1"
     const val koin = "3.0.2"
